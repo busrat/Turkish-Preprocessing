de187eed0a752bb2c7dc5f943867c80adba9137d
busrat <tabakbusra@gmail.com>
2020-11-28 02:53:45 +0300

    Update and rename tokenizer.py to tokenizerAndStopwordElimination.py

--- tokenizerAndStopwordElimination.py
+++ tokenizerAndStopwordElimination.py
@@ -9,11 +9,24 @@ def removeSomeCharacters(content):
         content = content.replace(i, '')
     return content
 
+stopwords = pd.read_csv('stopwords.txt', error_bad_lines=False, sep=',', header=None);
+def stopwordsElimination(content): 
+    if content in stopwords[0].values:
+        return True
+    else: 
+        return False
+        
 
+contentArray = []
 excelDF = pd.ExcelFile('news.xls')
 df1 = pd.read_excel(excelDF, sheet_name=0)
-for content in df1.content:
+for content in df1.content.values:
     content = removeSomeCharacters(content)
     content = splitter(content)
-    print(content)
+    for c in content:
+        if stopwordsElimination(c) == False:
+          contentArray.append(c)
+        
+    
+print(len(contentArray))
     
