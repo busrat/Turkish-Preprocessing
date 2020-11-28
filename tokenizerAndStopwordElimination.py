fac3031dac9c40eb76fdf1b8615fa6539b682b34
busrat <tabakbusra@gmail.com>
2020-11-28 12:29:51 +0300

    Update tokenizerAndStopwordElimination.py

--- tokenizerAndStopwordElimination.py
+++ tokenizerAndStopwordElimination.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from stopwordElimination import eliminate
 
 def splitter(content):
  return content.split()
@@ -9,24 +10,13 @@ def removeSomeCharacters(content):
         content = content.replace(i, '')
     return content
 
-stopwords = pd.read_csv('stopwords.txt', error_bad_lines=False, sep=',', header=None);
-def stopwordsElimination(content): 
-    if content in stopwords[0].values:
-        return True
-    else: 
-        return False
-        
-
-contentArray = []
+lastList = []
 excelDF = pd.ExcelFile('news.xls')
 df1 = pd.read_excel(excelDF, sheet_name=0)
-for content in df1.content.values:
-    content = removeSomeCharacters(content)
-    content = splitter(content)
-    for c in content:
-        if stopwordsElimination(c) == False:
-          contentArray.append(c)
-        
-    
-print(len(contentArray))
+contentArray = df1.content.values
+for content in contentArray:
+   content = removeSomeCharacters(content)
+   content = splitter(content)
+   content = eliminate(content)
+   lastList.append(content)    
     
