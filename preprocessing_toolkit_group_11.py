eb0e68fd889a4e40e0369c70eb150f5107939016
Esin Gedik <35924267+esingedik@users.noreply.github.com>
2020-12-24 21:39:27 +0300

    Add files via upload

--- preprocessing_toolkit_group_11.py
+++ preprocessing_toolkit_group_11.py
@@ -100,6 +100,54 @@ class SentenceSplitting:
         sentences = sentences[:-1]
         return sentences
 
+class TokenizationLogisticReg:
+    def __init__(self):
+        self.sents = nltk.corpus.treebank_raw.words()
+        self.tokens = []
+        self.boundaries = set()
+        self.offset = 0
+
+    def featureExtractor(self, tokens, i):
+        return {'isNextWordCapitalized': tokens[i][0].isupper(),
+                'previousWord': tokens[i - 1].lower(),
+                'nextWord': tokens[i + 1 if i < len(tokens) - 1 else 0].lower(),
+                'punctuation': tokens[i],
+                'isPreviousWordNumber': type(tokens[i - 1]) == int,
+                'isNextWordNumber': type(tokens[i + 1 if i < len(tokens) - 1 else 0]) == int}
+
+    def trainModel(self):
+        ozet = 'Şeker Portakalı , dünya klasikleri arasında en sevilerek okunan her yaşa hitap eden bir eserdir. Hayatınızın belli dönemlerinde farklı yaş gruplarında okuduğunuz da romanın sizin için anlamı ve değeri daha fazla değişecektir. Yazar Jose Mauro De Vasconceles bu eşsiz eserini on iki günde yazmıştır fakat biraz kendi hayatının bir kesiti gibi olan baş karakter Zeze bir ömür içinde taşıdığını söyler. Zeze aslında yazarın hayatının içerisinde anlam yüklediği tatlı karakteridir. TÜRÜ :  RomanYAZAR : Jose Mauro De Vasconceles KONUSU: Zeze , kalbi temiz ama çok yaramaz bir çocuktur. 5 yaşında olmasına rağmen Zeze hayat dolu , yaşına göre fazla zeki , yoksulluk içerisinde ki acı hayatını eser anlatmaktadır. Zeze çok yaramaz ve çok akıllı olduğu için çevresinde ki bir kaç insan hariç dışında ki herkes ona şeytan der. Zeze hatta o kadar zekidir ki okumayı erkenden söker ve öğretmeni tarafından çok sevilir. ÖNEMİ:  Şeker Portakalı yazarın bu serideki ilk kitabıdır. Eserin devamı niteliğinde ki ikinci kitabı ” Güneşi Uyandıralım ” dır. Serinin üçüncü romanı ise ” Delifişek”tir. Yazar Jose Mauro De Vasconceles bir çok farklı alanda iş yapmış ve çalışmıştır. Yazarlık cevherini ömrünün ileri ki yıllarında çok sonra keşfetmiştir. Bunun için bunca değiştiği meslek hayatı ve ömrünün birikmiş hikayeleri sayesinde romanlarını kaleme almıştır.Şeker Portakalı Roman Özeti Zeze ; 5 yaşında çok yaramaz fakat bir o kadar da yaşına göre fazla zeki bir çocuktur. Bu yaramazlığı ve zekiliğinden dolayı etrafındaki bir kaç kişi hariç herkes ona şeytan der. Onun şeytan olmadığına bir ablası bir de öğretmeni inanmaz , onu severler. Zekiliği sayesinde kendini öğretmenine sevdirmiştir çünkü okumayı erkenden sökmüştür. Zeze beş yaşında olmasına rağmen, bir yetişkin gibi davranıp olayları algılamaya çalışabiliyor. Babası işsiz olduğu için zor durumda olan aile; taşınmak zorunda kalır. Yoksulluk içerisinde yaşarken bir de bu taşınma olayı Zeze’yi üzer ve bir şeker portakalı seçmesini söylerler. Seçer ve bu şeker portakalı ; Zeze’nin en iyi arkadaşı sırdaşı olur. Her gün ne yaşadıysa , ne yaptıysa ona anlatmaya başlar karşılıklı sohbet ederler.Yılbaşı gelip çattığında Zeze babasından hediye bekler fakat yoksul baba hediye alamaz. Zeze’de babası da bu duruma üzülürler ama Zeze bu durumdan kendini suçlu hisseder . Bu yüzden ayakkabı boya kutusunu aldığı gibi dışarı çıkıp babasına hediye almak için para biriktirmeye başlar ve bunu başarır çok mutlu olur. Ama onun en büyük hayallerinden biri şehirde arabaların arkasına tutunup rüzgarı hissetmektir. Bu hayalini gerçekleştirir ama arabanın sahibi olan adamdan dayak yer. Orada araba sahibini bir gün öldüreceğine dair yemin eder. O adamı gördüğü yerde kaçar. Bir gün yine yaramazlık yaparken kendini keser. Okula giderken topallar tabii…  Adam Zeze’nin o halini görünce ona pansuman yaptırır ve limonata,pasta ısmarlar. Zeze ,adamı çok sever ilerleyen zamanda onu babası gibi görmeye başlar. Bu arada Zeze olağan yaramazlıklarına devam eder ve bunların sonucunda hep dayak yer. Bir gün çok ağır bir dayak yer ve o sırada birde bu Adamın arabasına tren çarpıp öldüğünü duyar. Ve Şeker Portakalı da kesilecektir artık Zeze iyice yıkılır. Fakat hayatına bu değerler olmadan artık devam etmek zorundadır.'
+        self.tokens = word_tokenize(ozet)
+        self.tokens.append("3.5")
+        featuresets = [(self.featureExtractor(self.tokens, i), (self.tokens[i] in ['.', ',', ':', ';']))
+                       for i in range(0, len(self.tokens) - 1)
+                       if any(x in self.tokens[i] for x in ['.', ',', ':', ';'])]
+        size = int(len(featuresets) * 0.5)
+        trainSet, testSet = featuresets[size:], featuresets[:size]
+        a, b = map(list, zip(*trainSet))
+        c, d = map(list, zip(*testSet))
+
+        pipe = make_pipeline(DictVectorizer(), LogisticRegressionCV(max_iter=1200000))
+        pipe.fit(a, b)
+        y_new = pipe.predict(c)
+        accuracy = metrics.accuracy_score(d, y_new)
+        return pipe, accuracy
+
+    def tokenizer(self, words):
+        pipe, accuracy = self.trainModel()
+        start = 0
+        sents = []
+        for i, word in enumerate(words):
+            extracted = self.featureExtractor(words, i)
+            if (any(x in word for x in ['.', ',', ':', ';']) and word not in ['.', ',', ':', ';']) and pipe.predict(extracted):
+                sents.extend(re.split("([.,:;])", word))
+                start = i + 1
+            else:
+                sents.append(word)
+        if start < len(words):
+            sents.append(words[start:])  # splitle ekle sadece boşluğa bakarak
+        return sents, accuracy
+
 class SentenceSplittingLogisticReg:
     def __init__(self):
         self.sents = nltk.corpus.treebank_raw.sents()
@@ -145,7 +193,7 @@ class SentenceSplittingLogisticReg:
         return {'isNextWordCapitalized': tokens[i][0].isupper(),
                 'previousWord': tokens[i - 1].lower(),
                 'punctuation': tokens[i],
-                'isPreviousWordIsSmallerThanFourChar': len(tokens[i-1]) <= 3}
+                'isPreviousWordIsOneChar': len(tokens[i - 1]) == 1}
 
 class Stemming:
     def __init__(self):
@@ -273,6 +321,14 @@ class Stemming:
         if word[index - 1] not in ["u", "ü", "ı", "i"]:
             return None, None
 
+        stem = word[:index]
+        suffix = word[index:]
+        if suffix in self.suffixesDict.keys():
+            if "FI" in self.suffixesDict[suffix]:
+                if stem in self.stemsDict.keys():
+                    if "FI" in self.stemsDict[stem]:
+                        return stem, suffix
+
         stem = word[:index - 1]
         if len(stem) > 2:
             stem, negativityIndex = self.checkNegativitySuffix(stem, index, word)
@@ -363,7 +419,7 @@ class Stemming:
                             return stemAndSuffixes, stem, stemType
 
             if stemType != "":
-                if ("EKSI" in stemType) and (stem[-1] in self.softeningLettersDict.values()):  # unsuz yumusamasi
+                if ("EKSI" in stemType) and (stem[-1] in self.softeningLettersDict.values()):  # ses dusmesi
                     for key in self.softeningLettersDict.keys():
                         if stem[-1] == self.softeningLettersDict[key]:
                             stem = stem[:-1] + key
@@ -513,9 +569,30 @@ class Initialization:
                 print("{} is not a correct choice. Please try again.".format(userInput))
 
     def CallTokenization(self):
-        text = self.GetTextFromUser("rule-based tokenization")
-        text = Tokenization().removeSomeCharacters(text)
-        print("\nTokenized text: ", text)
+        print("\nPlease choose a tokenization model using a or b keywords. Press 0 to return to the previous menu.\n")
+        print("\ta. Rule-based Model\n"
+              "\tb. Machine Learning Model\n"
+              "\t0. Return to the previous menu")
+
+        valid = ["0", "a", "b"]
+        while True:
+            userInput = input()
+            if userInput in valid:
+                if userInput == "0":
+                    main()
+                elif userInput == "a":
+                    text = self.GetTextFromUser("rule-based tokenization")
+                    text = Tokenization().removeSomeCharacters(text)
+                    print("\nTokenized text: ", text)
+                elif userInput == "b":
+                    text = self.GetTextFromUser("machine learning-based tokenization")
+                    tokenized, accuracy = TokenizationLogisticReg().tokenizer(text.split())
+                    print("\nTokenized Sentence:", tokenized)
+                    print("\nAccuracy of the Logistic Regression model: %.2f" % accuracy)
+                break
+            else:
+                print("{} is not a correct choice. Please try again.".format(userInput))
+                continue
         self.CheckContinuity()
 
     def CallStopWordElimination(self):
@@ -602,6 +679,7 @@ class Initialization:
     def CallNormalization(self):
         text = self.GetTextFromUser("normalization")
         text = Tokenization().removeSomeCharacters(text)
+        print("\nWord suggestions after normalization:")
         for word in text:
             if not any(c.isdigit() for c in word):
                 normalizedList = Normalization().checkInputWord(word)
